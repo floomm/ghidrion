79050c85d3e95bef6331a3b17843526e49d62eba
Silvan Flum <flumsil1@students.zhaw.ch>
2023-04-12 15:39:48 +0200

    Move MorionTraceColorizerScript.java

--- Ghidrion/src/main/java/ghidra_scripts/MorionTraceColorizerScript.java
+++ Ghidrion/src/main/java/ghidra_scripts/MorionTraceColorizerScript.java
@@ -1,3 +1,5 @@
+package ghidra_scripts;
+
 //Colors a recorded Morion trace in the Ghidra Listing window.
 //@author Silvan Flum
 //@category Morion
@@ -6,6 +8,10 @@
 //@toolbar
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import org.yaml.snakeyaml.Yaml;
 
 import ghidra.app.script.GhidraScript;
 import ghidra.util.Msg;
@@ -25,10 +31,15 @@ public class MorionTraceColorizerScript extends GhidraScript {
         
         // Analyze the selected file
         analyzeFile(selectedFile);
+        
+        InputStream input = new FileInputStream(selectedFile);
+        Yaml yaml = new Yaml();
+        Object data = yaml.load(input);
 	}
 
     private void analyzeFile(File file) {
         // Add your code here to analyze the selected file
         Msg.info(this, "Analyzing file: " + file.getAbsolutePath());
     }
+    
 }
