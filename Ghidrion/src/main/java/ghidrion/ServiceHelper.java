7e309e250e8bedbe006906bef0944986017bede5
Silvan Flum <flumsil1@students.zhaw.ch>
2023-04-25 15:56:36 +0200

    Update ServiceHelper.java

--- Ghidrion/src/main/java/ghidrion/ServiceHelper.java
+++ Ghidrion/src/main/java/ghidrion/ServiceHelper.java
@@ -1,14 +1,17 @@
 package ghidrion;
 
+import java.awt.Component;
+
 import ghidra.framework.plugintool.PluginTool;
+import ghidra.util.Msg;
 
 public class ServiceHelper {
 
-	public static <T> T getService(PluginTool tool, Class<T> c) {
+	public static <T> T getService(PluginTool tool, Class<T> c, Object originator, Component parent) {
 		T service = tool.getService(c);
 		if (service == null) {
-			// Handle the case where the service is not available
-            throw new RuntimeException(c.getName() + " not found");
+			String name = c.getName();
+			Msg.showError(originator, parent, "No " + name, "Can't find " + name);
 		}
 		
 		return service;
