b3c09286eee26044f66e33b13a4a96f47f74ff1c
Silvan Flum <flumsil1@students.zhaw.ch>
2023-04-26 14:15:25 +0200

    Move build of DisplayTracePanel to separate class

--- Ghidrion/src/main/java/view/GhidrionProvider.java
+++ Ghidrion/src/main/java/view/GhidrionProvider.java
@@ -1,36 +1,20 @@
 package view;
 
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.List;
 
-import javax.swing.DefaultListModel;
-import javax.swing.JButton;
-import javax.swing.JColorChooser;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
-import javax.swing.JList;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 
 import docking.ComponentProvider;
 import ghidrion.GhidrionPlugin;
-import ghidrion.MorionTraceColorizer;
 
 public class GhidrionProvider extends ComponentProvider {
 
 	private GhidrionPlugin plugin;
 	
 	private JPanel panel;
-	private DefaultListModel<String> traceListModel = new DefaultListModel<>();
-	private JList<String> traceList = new JList<>(traceListModel);
-	
-	private MorionTraceColorizer colorizer;
-	private Color traceColor = Color.CYAN;
+	private DisplayTracePanel displayTracePanel;
 
 	public GhidrionProvider(GhidrionPlugin plugin, String pluginName, String owner) {
 		super(plugin.getTool(), pluginName, owner);
@@ -40,7 +24,7 @@ public class GhidrionProvider extends ComponentProvider {
 	}
 	
 	public void init() {
-		this.colorizer = new MorionTraceColorizer(plugin, panel);
+		displayTracePanel.init(plugin);
 	}
 
 	// Customize GUI
@@ -51,87 +35,10 @@ public class GhidrionProvider extends ComponentProvider {
 		panel.add(new JLabel("Trace an execution"));
 		panel.add(new JPanel());
 		panel.add(new JLabel("Display Morion trace file"));
-		panel.add(buildDisplayTracePanel());
+		displayTracePanel = new DisplayTracePanel();
+		panel.add(displayTracePanel);
 		
 		setVisible(true);
-	}
-	
-	private JPanel buildDisplayTracePanel() {
-		JPanel displayTracePanel = new JPanel(new GridLayout(2, 1));
-		
-		JPanel panel1 = new JPanel(new FlowLayout());
-		panel1.add(buildDisplayTraceButton());
-		panel1.add(buildChooseColorButton());
-		panel1.add(buildRemoveTracesButton());
-		
-		displayTracePanel.add(panel1);
-		displayTracePanel.add(buildTracesScrollPane());
-		
-		return displayTracePanel;
-	}
-	
-	/**
-	 * @return Button which allows to choose a trace color
-	 */
-	private JButton buildChooseColorButton() {
-		JButton colorButton = new JButton();
-
-		colorButton.setBackground(traceColor);
-		colorButton.setPreferredSize(new Dimension(25, 25));
-		colorButton.setMinimumSize(new Dimension(20, 20));
-
-		colorButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-                Color newColor = JColorChooser.showDialog(panel, "Choose a color", traceColor);
-                if (newColor != null) {
-                    traceColor = newColor;
-                    colorButton.setBackground(traceColor);
-                    colorButton.setOpaque(true);
-                }
-			}
-		});
-		
-		return colorButton;
-	}
-	
-	private JButton buildDisplayTraceButton() {
-		JButton importButton = new JButton("Display trace");
-		
-		importButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				String traceName = colorizer.colorize(traceColor);
-				traceListModel.addElement(traceName);
-			}
-		});
-		
-		return importButton;
-	}
-	
-	private JButton buildRemoveTracesButton() {
-		JButton removeButton = new JButton("Remove selected traces");
-		
-		removeButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				List<String> selectedItems = traceList.getSelectedValuesList();
-				colorizer.decolorize(selectedItems);
-
-				int[] selectedIndices = traceList.getSelectedIndices();
-				for (int i : selectedIndices) {
-					traceListModel.remove(i);
-				}
-			}
-		});
-		
-		return removeButton;
-	}
-	
-	private JScrollPane buildTracesScrollPane() {
-		JScrollPane tracesScrollPane = new JScrollPane(traceList);
-		
-		return tracesScrollPane;
 	}
 
 	@Override
